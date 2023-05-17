3e2caa06a8331dc3a59a08aea06ad3ce11955f73
Wendel.Miguel <wendel.miguel@targetsistemas.com.br>
2023-05-17 16:11:14 -0300

    Adicionando botao de cadastro e logo na tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'main_page.dart';
+import 'telas/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
--- lib/telas/main_screen.dart
+++ lib/telas/main_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:teadloginpage/sign_up_form.dart';
-import 'animation_button_to_form.dart';
-import 'login_form.dart';
-import 'utils.dart';
+import '../animation_button_to_form.dart';
+import '../login_form.dart';
+import '../uteis/utils.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -10,19 +10,31 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
+        body: Stack(
+      alignment: Alignment.center,
       children: [
-        const ButtonToLoginForm(
-            formHeight: Utils.loginFormHeight,
-            childWidget: LoginForm(),
-            buttonText: Text("LOGIN", style: Utils.buttonTextStyle),
-            color: Colors.red),
+        Positioned(
+          top: 150,
+          child: Image.asset(
+            "assets/images/arvore_logo_tead_600X600.png",
+            width: 200,
+            height: 200,
+          ),
+        ),
+        const Positioned(
+          bottom: 180,
+          child: ButtonToLoginForm(
+              formHeight: Utils.loginFormHeight,
+              childWidget: LoginForm(),
+              buttonText: Text("LOGIN", style: Utils.buttonTextStyle),
+              color: Colors.red),
+        ),
         ButtonToLoginForm(
           formHeight: Utils.signUpFormHeight,
           childWidget: SignUpButton(),
           buttonText: const Text("CADASTRE-SE", style: Utils.buttonTextStyle),
           color: Colors.grey,
-        )
+        ),
       ],
     ));
   }
